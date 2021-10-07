401113051baf33ea3e1fbe16283046146e91d2ea
Priscila Duarte <duartecostap@gmail.com>
2021-10-07 11:30:57 -0300

    Sobe classes ts

---
+++ Typescript/fundamentos-typescript/arrays-iteracoes.ts
@@ -0,0 +1,11 @@
+let notasAlunos: number[] = [10, 8, 8.5, 7];
+
+//indices -in
+for (let i in notasAlunos){
+    console.log(i)
+}
+
+//valores -of
+for (let notaAlunos of notasAlunos){
+    console.log(notaAlunos);
+}
