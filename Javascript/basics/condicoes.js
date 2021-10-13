8a455b6b4b3a7d97516e2f5e09ab4472cf3d2554
Priscila Duarte <duartecostap@gmail.com>
2021-10-13 15:17:06 -0300

    up update

---
+++ Javascript/basics/condicoes.js
@@ -0,0 +1,19 @@
+var teste = true;
+var x = 1;
+if( teste == true){
+    console.log("Acertou!")
+}else{
+    console.log("Errou!")
+}
+
+switch (x) {
+    case 11:
+        console.log("parou ganhou")
+        break;
+    case 3:
+        console.log("Nada disso")
+        break;
+    default:
+        console.log("Nada encontrado")
+        break;
+}
---
+++ Javascript/basics/script.js
@@ -0,0 +1,7 @@
+var x = 15; var y = 12; var z; z=30; z=12; var w; var pessoa = null;
+
+console.log( x + y)
+console.log(z)
+console.log(w)
+console.log(pessoa)
+console.log('Hello World Again!');
