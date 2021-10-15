b99b5581cfce73f23b9a4014fbc2bfea76dac581
Priscila Duarte <duartecostap@gmail.com>
2021-10-15 16:12:29 -0300

    Starks

---
+++ Javascript/basics/arrays.js
@@ -0,0 +1,8 @@
+var stark = [
+    ["ned", 40],
+    ["Jon", 16],
+    ["Rickon", 3]
+]
+
+console.log(stark[2])
+console.log("teste isso")
---
+++ Javascript/basics/prototype.js
@@ -0,0 +1,26 @@
+/* function Stark(nome, idade, corDoCabelo) {
+  this.nome = nome;
+  this.idade = idade;
+  this.corDoCabelo = corDoCabelo;
+
+  this.sobrenome = "Stark";
+  this.apresentar = function () {
+    console.log("Olá, eu sou " + this.nome + " " + this.sobrenome + ".");
+  };
+}
+
+var ned = new Stark("Ned", 40, "Preto");
+ned.apresentar();
+var sansa = new Stark("Sansa", 13, "Cobre");
+sansa.apresentar();
+
+console.log(ned);
+console.log(sansa);
+ */
+
+String.prototype.apagar = function () {
+    return "";
+}
+
+console.log("oi".apagar());
+
