87039c142e8de2e6221b387482c785c4be737300
prisciladuarte <duartecostap@gmail.com>
2021-11-17 14:32:48 -0300

    Operações

---
+++ Workshop - XPJS/tipo2.js
@@ -0,0 +1,8 @@
+0 != false
+
+if(0) {
+    console.log('0 é true');
+}else {
+    console.log('0 é false');
+}
+
---
+++ Workshop - XPJS/typeof.js
@@ -0,0 +1,12 @@
+var ativo = true;
+
+var segredo = [];
+
+console.log("ativo: ", typeof ativo);
+console.log("segredo: ", typeof segredo);
+
+/* ------------------------------------------------ */
+
+var soma = 4 + 3
+
+console.log(soma);
