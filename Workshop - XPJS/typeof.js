bde78db2357d62691c660a4a0beef131eda90467
prisciladuarte <duartecostap@gmail.com>
2021-11-17 14:35:41 -0300

    go

--- Workshop - XPJS/typeof.js
+++ Workshop - XPJS/typeof.js
@@ -10,3 +10,12 @@ console.log("segredo: ", typeof segredo);
 var soma = 4 + 3
 
 console.log(soma);
+
+var a = uva
+var b = uva
+
+if(a == b){
+    console.log("Você quer mesmo comprar fruta repetida nesse natal?");
+}else{
+    console.log("adoro variedade");
+}
